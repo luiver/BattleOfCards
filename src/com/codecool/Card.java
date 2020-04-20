d3b087bcbffee318e90a04bdbc5a6102e7af6fad
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-04-20 19:56:59 +0200

    Card class and CardParser class add, parse function add, getters/setters add

---
+++ src/com/codecool/Card.java
@@ -0,0 +1,33 @@
+package com.codecool;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Card {
+    private String id;
+    private String cardName;
+    private Map<String, Integer> stats;
+
+    public Card(String id, String cardName) {
+        this.id = id;
+        this.cardName = cardName;
+        this.stats = new HashMap<>();
+    }
+
+    public Set<String> getIdSet() {
+        return this.stats.keySet();
+    }
+
+    public void setCardStatsById(String id, int value) {
+        this.stats.put(id, value);
+    }
+
+    public int getValueById(String id) {
+        return this.stats.get(id);
+    }
+
+    public String getCardName() {
+        return this.cardName;
+    }
+}
---
+++ src/com/codecool/CardParser.java
@@ -0,0 +1,72 @@
+package com.codecool;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class CardParser {
+    private Deck deck;
+    private Document doc;
+
+    public CardParser(){
+        this.deck = new Deck();
+        parseCards();
+    }
+
+    protected void loadXmlDocument(String path) {
+        try {
+            File file = new File(path);
+            DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance()
+                    .newDocumentBuilder();
+            this.doc = dBuilder.parse(file);
+            this.doc.getDocumentElement().normalize();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public Document getDoc() {
+        return this.doc;
+    }
+
+    private void parseCards() {
+        this.loadXmlDocument("Cards.xml");
+        NodeList nlist = doc.getElementsByTagName("Card");
+
+        for (int i = 0; i < nlist.getLength(); i++) {
+            Node node = nlist.item(i);
+
+            if (node.getNodeType() != Node.ELEMENT_NODE) {
+                continue;
+            }
+
+            Element element = (Element) node;
+            String id = element.getAttribute("id");
+
+            NodeList nodeList = doc.getElementsByTagName("Description");
+
+            Node node2 = nodeList.item(i);
+            Element element2 = (Element) node2;
+            String cardName = element2.getAttribute("value");
+
+
+            Card card = new Card(id, cardName);
+            addStatsToCard(card, element);
+
+            this.deck.addCard(card);
+        }
+    }
+
+    private void addStatsToCard(Card card, Element element) {
+        Element evalsNode = (Element) element.getElementsByTagName("Evals").item(0);
+        NodeList eval = evalsNode.getElementsByTagName("Eval");
+
+        for (int i = 0; i < eval.getLength(); i++) {
+            Element evalElement = (Element) eval.item(i);
+            String id = evalElement.getAttribute("id");
+            int value = Integer.parseInt(evalElement.getTextContent());
+            card.setCardStatsById(id, value);
+        }
+    }
+}
