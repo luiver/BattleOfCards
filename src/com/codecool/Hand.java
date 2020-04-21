be920e04d86c0ab78e5edbbc3b7af61eea8b3180
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-04-21 11:35:18 +0200

    Hand Class with iterator class add, sout debug line in CardParser class add

---
+++ src/com/codecool/Hand.java
@@ -0,0 +1,41 @@
+package com.codecool;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Hand {
+    private List<Card> cardsOnHand;
+    private Iterator<Card> cardIterator;
+
+    public Hand() {
+        this.cardsOnHand = new ArrayList<>();
+        this.cardIterator = new CardIterator();
+    }
+
+    public void addCard(Card card) {
+        this.cardsOnHand.add(card);
+    }
+
+    private class CardIterator implements Iterator<Card> {
+        int index;
+
+        @Override
+        public boolean hasNext() {
+            return index < cardsOnHand.size();
+        }
+
+        @Override
+        public Card next() {
+            if (this.hasNext()) {
+                return cardsOnHand.get(index++);
+            } else {
+                return null;
+            }
+        }
+    }
+
+    public Iterator<Card> getIterator() {
+        return this.cardIterator;
+    }
+}
